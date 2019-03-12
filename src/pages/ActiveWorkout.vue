0f15848706e2ac0575579b4b8138cd3d4e56ae20
David Thomason <dtom90@users.noreply.github.com>
2019-03-11 20:54:31 -0400

    Updated rest/exercise logic

--- src/pages/ActiveWorkout.vue
+++ src/pages/ActiveWorkout.vue
@@ -7,13 +7,13 @@
       <p>{{workout.exercises}} x {{workout.rounds}}</p>
 
       <f7-block>
-        <h1>Current Round: {{ currentRound }}</h1>
-        <h1>Current Exercise: {{ currentExercise }}</h1>
-        <p>Time Remaining: {{ countdown }}</p>
+        <h1>{{ rest ? 'Next' : 'Current' }} Round: {{ currentRound }}</h1>
+        <h1>{{ rest ? 'Next' : 'Current' }} Exercise: {{ currentExercise }}</h1>
+        <p>Rest: {{ countdown }}</p>
       </f7-block>
 
-      <f7-button v-if="!done" class="col" big fill raised color="green" @click="goToNextExercise">
-        Next
+      <f7-button v-if="!done" class="col" big fill raised color="green" @click="next">
+        {{ rest ? 'Skip Rest' : 'Complete' }}
       </f7-button>
       <h1 v-if="done">DONE!!!</h1>
 
@@ -23,35 +23,47 @@
 </template>
 
 <script>
-const interval = 2;
+const interval = 3;
 
 export default {
+
   data: () => ({
     workout: {},
     currentExerciseIndex: 0,
     currentRound: 1,
     countdown: interval,
     timer: null,
+    started: false,
+    rest: false,
     done: false
   }),
+
   created: function () {
     this.workout = this.$store.state.workouts.filter(w => w.id === this.$f7route.params['workoutId'])[0];
-    this.timer = setInterval(this.decrementCountdown, 1000)
   },
+
   computed: {
     currentExercise() { return this.workout.exercises[this.currentExerciseIndex] },
   },
+
   methods: {
 
-    goToNextExercise() {
+    next() {
 
-      if (this.currentExerciseIndex === this.workout.exercises.length-1 &&
-        this.currentRound === this.workout.rounds) {
-        this.done = true
-      } else {
-        this.currentExerciseIndex = (this.currentExerciseIndex + 1) % this.workout.exercises.length;
-        if (this.currentExerciseIndex === 0)
-          this.currentRound += 1
+      if(!this.rest) {
+
+        this.rest = true;
+        this.countdown = interval;
+        this.timer = setInterval(this.decrementCountdown, 1000);
+
+        if (this.currentExerciseIndex === this.workout.exercises.length-1 &&
+          this.currentRound === this.workout.rounds) {
+          this.done = true
+        } else {
+          this.currentExerciseIndex = (this.currentExerciseIndex + 1) % this.workout.exercises.length;
+          if (this.currentExerciseIndex === 0)
+            this.currentRound += 1
+        }
       }
     },
 
@@ -59,13 +71,9 @@ export default {
       if(this.countdown > 1) {
         this.countdown -= 1;
       } else {
-        this.goToNextExercise();
-        if (this.done) {
-          this.countdown -= 1;
-          clearInterval(this.timer);
-        }
-        else
-          this.countdown = interval
+        this.countdown -= 1;
+        this.rest = false;
+        clearInterval(this.timer);
       }
     }
 
