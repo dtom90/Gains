5b94ca34f5fc88665993aa1c0d5a1c7232693de5
David Thomason <dtom90@users.noreply.github.com>
2019-03-23 14:05:15 -0400

    Added .travis.yml, .nvmrc, npm lint. Fixed eslint errors in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
 <template>
   <!-- App -->
-  <f7-app :params="f7params" theme-dark>
-
+  <f7-app
+    :params="f7params"
+    theme-dark
+  >
     <!-- Main View -->
-    <f7-view id="main-view" url="/" main></f7-view>
-
+    <f7-view
+      id="main-view"
+      url="/"
+      main
+    />
   </f7-app>
 </template>
 
 <script>
+import { f7App, f7View } from 'framework7-vue'
 // Import Routes
 import routes from './routes.js'
 
 export default {
-  data() {
+  components: { f7App, f7View },
+  data () {
     return {
       // Framework7 parameters here
       f7params: {
@@ -21,8 +28,8 @@ export default {
         name: 'Framework7', // App name
         theme: 'auto', // Automatic theme detection
         // App routes
-        routes: routes,
-      },
+        routes: routes
+      }
     }
   }
 }
