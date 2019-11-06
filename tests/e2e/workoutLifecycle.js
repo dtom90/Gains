46f656e6742b6e23e832bec618d407050cfeb8c6
David Thomason <dtom90@users.noreply.github.com>
2019-11-06 12:42:03 -0500

    Abstracted an inputGroup in e2e

--- tests/e2e/workoutLifecycle.js
+++ tests/e2e/workoutLifecycle.js
@@ -8,8 +8,12 @@ const page = `http://${hostname}:${port}${path}`
 const blockTitle = text => Selector('div.block-title').withExactText(text)
 const title = text => Selector('div.title').withExactText(text)
 const button = text => Selector('a.button').withExactText(text)
-const inputLabel = label => Selector('div.item-label').withExactText(label)
-const input = ({ type, placeholder }) => Selector(`input[type="${type}"]${placeholder ? `[placeholder="${placeholder}"]` : ''}`)
+const inputCss = ({ type, placeholder }) => `input[type="${type}"]${placeholder ? `[placeholder="${placeholder}"]` : ''}`
+const input = ({ type, placeholder }) => Selector(inputCss({ type, placeholder }))
+const inputGroup = (label, { type, placeholder }) => Selector('li')
+  .find('div.item-label').withExactText(label)
+  .parent('li')
+  .find(inputCss({ type, placeholder }))
 const listItem = text => Selector('li').withExactText(text)
 
 fixture(`Testing Gains`)
@@ -21,34 +25,30 @@ fixture(`Testing Gains`)
   })
 
 test('Create a workout', async t => {
+  const workoutNameInput = inputGroup('Name', { type: 'text', placeholder: 'Workout Name' })
+  const exerciseNameInput = i => inputGroup('Exercise ' + i, { type: 'text', placeholder: 'Exercise Name' })
+
   await t
     //
-    // Open the New Workout page
+    // Open the New Workout page, expect the right content
     .click(button('New Workout'))
     .expect(title('New Workout').visible).ok()
-    .expect(inputLabel('Name').visible).ok()
-    .expect(input({ type: 'text', placeholder: 'Workout Name' }).visible).ok()
-    .expect(inputLabel('Exercise 1').visible).ok()
-    .expect(input({ type: 'text', placeholder: 'Exercise Name' }).visible).ok()
+    .expect(workoutNameInput.visible).ok()
+    .expect(exerciseNameInput('1').visible).ok()
     .expect(input({ type: 'text', placeholder: 'Exercise Name' }).count).eql(1)
-    .expect(inputLabel('Target weight:').exists).notOk()
-    .expect(input({ type: 'number', placeholder: '0 (bodyweight)' }).exists).notOk()
-    .expect(inputLabel('Target reps:').exists).notOk()
-    .expect(input({ type: 'number', placeholder: '(unset)' }).exists).notOk()
-    .expect(inputLabel('Rounds').visible).ok()
-    .expect(input({ type: 'number' }).visible).ok()
-    .expect(input({ type: 'number' }).value).eql('1')
+    .expect(inputGroup('Target weight:', { type: 'number', placeholder: '0 (bodyweight)' }).exists).notOk()
+    .expect(inputGroup('Target reps:', { type: 'number', placeholder: '(unset)' }).exists).notOk()
+    .expect(inputGroup('Rounds', { type: 'number' }).visible).ok()
+    .expect(inputGroup('Rounds', { type: 'number' }).value).eql('1')
     //
     // Type in the workout parameters
-    .typeText(input({ type: 'text', placeholder: 'Workout Name' }), 'My Workout')
-    .typeText(input({ type: 'text', placeholder: 'Exercise Name' }).nth(0), 'Push-ups')
-    .expect(inputLabel('Target weight:').visible).ok()
-    .expect(input({ type: 'number', placeholder: '0 (bodyweight)' }).visible).ok()
-    .expect(inputLabel('Target reps:').visible).ok()
-    .expect(input({ type: 'number', placeholder: '(unset)' }).visible).ok()
-    .expect(inputLabel('Exercise 2').visible).ok()
+    .typeText(workoutNameInput, 'My Workout')
+    .typeText(exerciseNameInput('1'), 'Push-ups')
+    .expect(inputGroup('Target weight:', { type: 'number', placeholder: '0 (bodyweight)' }).visible).ok()
+    .expect(inputGroup('Target reps:', { type: 'number', placeholder: '(unset)' }).visible).ok()
+    .expect(exerciseNameInput('2').visible).ok()
     .expect(input({ type: 'text', placeholder: 'Exercise Name' }).count).eql(2)
-    .typeText(input({ type: 'text', placeholder: 'Exercise Name' }).nth(1), 'Pull-ups')
+    .typeText(exerciseNameInput('2'), 'Pull-ups')
     .click(button('Create Workout'))
     //
     // Should navigate to the workout page
