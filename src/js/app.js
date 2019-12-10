8f2eca36bebba672b5b3dc28571d6f10d10116aa
David Thomason <dtom90@users.noreply.github.com>
2019-12-10 08:16:16 -0500

    Merged app.js & app.scss, ran yarn, moved .eslintrc.yml into /src

--- src/js/app.js
+++ src/js/app.js
@@ -1,32 +1,36 @@
-// Import Vue
-import Vue from 'vue';
+// Import Vue & Vuex store
+import Vue from 'vue'
+import store from '../store'
 
 // Import Framework7
-import Framework7 from 'framework7/framework7-lite.esm.bundle.js';
+import Framework7 from 'framework7'
 
 // Import Framework7-Vue Plugin
-import Framework7Vue from 'framework7-vue/framework7-vue.esm.bundle.js';
+import Framework7Vue from 'framework7-vue'
+
+// Import additional Framework7 components
+import Picker from 'framework7/components/picker/picker'
+import Sheet from 'framework7/components/sheet/sheet'
+import Dialog from 'framework7/components/dialog/dialog'
+import Panel from 'framework7/components/panel/panel'
 
 // Import Framework7 Styles
-import 'framework7/css/framework7.bundle.css';
+import 'framework7/css/framework7.bundle.css'
 
 // Import Icons and App Custom Styles
-import '../css/icons.css';
-import '../css/app.scss';
+import '../css/icons.css'
+import '../css/app.scss'
 
 // Import App Component
-import App from '../components/app.vue';
+import App from '../components/app.vue'
 
-// Init Framework7-Vue Plugin
-Framework7.use(Framework7Vue);
+// Init Framework7-Vue Plugin and additional components
+Framework7.use([Framework7Vue, Dialog, Picker, Sheet, Panel])
 
 // Init App
-new Vue({
+new Vue({ // eslint-disable-line no-new
   el: '#app',
-  render: (h) => h(App),
-
-  // Register App Component
-  components: {
-    app: App
-  },
-});
+  components: { App },
+  template: '<App/>',
+  store // Use Vuex store
+})
